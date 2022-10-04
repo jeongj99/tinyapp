f11b99e74485f5e934c42bffc362f2197504081d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-04 00:29:04 +0000

    Added a GET route and rendered it with a template, and added a POST route to receive from submissions

--- express_server.js
+++ express_server.js
@@ -1,3 +1,7 @@
+const generateRandomString = () => {
+  
+}
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -8,6 +12,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -24,6 +30,15 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+})
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:id', (req, res) => {
