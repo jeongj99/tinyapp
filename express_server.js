ebc88f32aa84ce03da3c4d4594b2b80343e66b65
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-05 04:19:54 +0000

    Added comments that describe each GET or POST route

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+// Function that generates the short URL; used in the /urls/new POST route
 const generateRandomString = () => {
   let string = '';
   const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -23,18 +24,17 @@ const urlDatabase = {
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// GET route for '/', where it just redirects to /urls
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.redirect('/urls');
 });
 
+// GET route for '/urls.json' shows a JSON object literal of urlDatabase
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
+// GET route for '/urls', where it displays the an html browser using the urls_index.ejs
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
@@ -43,27 +43,32 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// POST route for '/login', where it creates a cookie with the username upon request
 app.post('/login', (req, res) => {
   res.cookie('username', req.body.username);
   res.redirect('/urls');
 });
 
+// POST route for '/logout', where it deletes the cookie with the username upon request
 app.post('/logout', (req, res) => {
   res.clearCookie('username');
   res.redirect('/urls');
 })
 
+// POST route for '/urls', where a new short URL with its long URL is added to database and displayed in the page
 app.post('/urls', (req, res) => {
   const id = generateRandomString();
   urlDatabase[id] = req.body.longURL;
   res.redirect(`/urls/${id}`);
 });
 
+// GET route for '/urls/new', where it displays a page where one can submit a new url. It uses the urls_new.ejs template
 app.get('/urls/new', (req, res) => {
   const templateVars = { username: req.cookies['username']}
   res.render('urls_new', templateVars);
 });
 
+// Get route for '/urls/:id', where id parameter is the short URL. urls_show.ejs used for template
 app.get('/urls/:id', (req, res) => {
   const templateVars = {
     id: req.params.id,
@@ -73,17 +78,20 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+// POST route for 'urls/:id', where it edits the long URL in the database and the change is displayed in the /url page upon request
 app.post('/urls/:id', (req, res) => {
   const id = req.params.id;
   urlDatabase[id] = req.body.editedLongURL;
   res.redirect('/urls');
 });
 
+// POST route for '/urls/:id/delete', where it upon request deletes the selected url from the database and the change is displayed in /urls
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
 
+// GET route for /u/:id, where it just redirects you the actual destination of the long URL
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
